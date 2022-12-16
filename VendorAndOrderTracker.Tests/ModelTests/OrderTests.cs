cf9c8a5f6392d42dd9e7f27617a083b610306126
Dominik Magic <dominikmagic5@gmail.com>
2022-12-16 14:38:15 -0800

    Test 6 for Order method added

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -62,5 +62,18 @@ namespace VendorAndOrderTracker.Tests
       Order newOrder = new Order(titleInput, descriptionInput, priceInput, dateInput);
       Assert.AreEqual(dateInput, newOrder.Date);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsListOfAllOrderObjects_OrderList()
+    {
+      string titleInput = "Bread";
+      string descriptionInput = "4 loafs of bread";
+      int priceInput = 15;
+      DateTime dateInput = new DateTime(2022, 12, 16);
+      Order newOrder = new Order(titleInput, descriptionInput, priceInput, dateInput);
+      List<Order> orderList = new List<Order>{newOrder};
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(result, orderList);      
+    }
   }
 }
