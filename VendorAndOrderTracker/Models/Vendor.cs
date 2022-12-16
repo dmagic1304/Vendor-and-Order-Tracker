8f6366167180d3b128ea7f2de5b36c1f37ebb573
Dominik Magic <dominikmagic5@gmail.com>
2022-12-16 09:33:58 -0800

    Test 4 passed

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -1,14 +1,19 @@
+using System.Collections.Generic;
+
 namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
     public string Name { get; set;}
     public string Description { get; set;}
+    public List<Vendor> Orders { get; set;}
 
     public Vendor (string name, string description)
     {
       Name = name;
       Description = description;
+      Orders = new List<Vendor> {};
     }
+    
   }
 }
