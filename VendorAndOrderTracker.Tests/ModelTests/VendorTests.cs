e5224ce9cc9a4a44f8d93392c9ce97791c932b38
Dominik Magic <dominikmagic5@gmail.com>
2022-12-16 08:45:22 -0800

    Created Vendor class model and test folder/file

---
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VendorAndoRderTracker.Models;
+
+namespace VendorAndoRderTracker.TestTools
+{
+  [TestClass]
+  public class VendorTests
+  {
+    
+  }
+}
