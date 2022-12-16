b13b6a66d9c84cffd0035650f5959c6b9dba622b
Dominik Magic <dominikmagic5@gmail.com>
2022-12-16 12:11:35 -0800

    It will offer add new vendor link on vendor page, display vendor add form and bring back to homepage upon submit

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -12,5 +12,18 @@ namespace VendorAndOrderTracker.Controllers
       List<Vendor> vendorList = Vendor.GetAll();
       return View(vendorList);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index", "Home");
+    }
   }
 }
