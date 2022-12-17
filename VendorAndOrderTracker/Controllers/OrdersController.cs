f6623eb69354ad414859f2f7ec2a2ce11643a5b2
Dominik Magic <dominikmagic5@gmail.com>
2022-12-16 16:25:49 -0800

    added an option to delete individual order

--- VendorAndOrderTracker/Controllers/OrdersController.cs
+++ VendorAndOrderTracker/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.Controllers
@@ -15,8 +16,21 @@ namespace VendorAndOrderTracker.Controllers
     [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
     public ActionResult Show(int vendorId, int orderId)
     {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor vendor = Vendor.Find(vendorId);
       Order order = Order.Find(orderId);
-      return View(order);
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Destroy(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      vendor.Orders.Remove(order);
+      return RedirectToAction("Show", "Vendors", vendor);
     }
 
   } 
